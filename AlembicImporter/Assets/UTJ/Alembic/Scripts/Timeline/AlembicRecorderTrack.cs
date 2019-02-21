a526f33811a2e55b98c0d2ab762b43ea8693cfde
afterwise <afterwise@gmail.com>
2019-02-21 09:54:30 +0100

    Remove deprecated things

--- AlembicImporter/Assets/UTJ/Alembic/Scripts/Timeline/AlembicRecorderTrack.cs
+++ AlembicImporter/Assets/UTJ/Alembic/Scripts/Timeline/AlembicRecorderTrack.cs
@@ -5,7 +5,9 @@ namespace UTJ.Alembic
 {
     [System.Serializable]
     [TrackClipType(typeof(AlembicRecorderClip))]
+#if !UNITY_2018_2_OR_NEWER
     [TrackMediaType(TimelineAsset.MediaType.Script)]
+#endif
     [TrackColor(0.33f, 0.0f, 0.08f)]
     public class AlembicRecorderTrack : TrackAsset
     {
--- AlembicImporter/Assets/UTJ/Alembic/Scripts/Timeline/AlembicTrack.cs
+++ AlembicImporter/Assets/UTJ/Alembic/Scripts/Timeline/AlembicTrack.cs
@@ -5,7 +5,9 @@ namespace UTJ.Alembic
 {
     [System.Serializable]
     [TrackClipType(typeof(AlembicShotAsset))]
+#if !UNITY_2018_2_OR_NEWER
     [TrackMediaType(TimelineAsset.MediaType.Script)]
+#endif
     [TrackColor(0.53f, 0.0f, 0.08f)]
     public class AlembicTrack : TrackAsset
     {
